cf5ef5a77d75682836720240abf410e10d0484be
Lego Panda <legopanda2026@gmail.com>
2026-02-18 18:30:22 +0200

    Create robot_conf.py

---
+++ robot_conf.py
@@ -0,0 +1,27 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop, Axis
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch, hub_menu
+
+hub = PrimeHub(Axis.Y, Axis.Z)
+
+COLOR_PRESETS = {
+    "stopYellow": (Color.YELLOW, Color.GREEN),
+    "stopRed":    (Color.RED, Color.BLUE),
+    "stopBlue":   (Color.BLUE, Color.YELLOW),
+    "stopGreen":  (Color.GREEN, Color.RED)
+}
+
+
+leftwheel = Motor(Port.A, Direction.CLOCKWISE)
+rightwheel = Motor(Port.B, Direction.CLOCKWISE)
+
+shell = Motor(Port.E)
+arm = Motor(Port.C)
+colorS = ColorSensor(Port.D)
+
+wheels = DriveBase(leftwheel, rightwheel, 62.4, 100)
+
+CIRCUMFERENCE = 18.89
+SHELL_RATIO = 360 / 1795
