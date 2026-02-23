0c83c67c238d233f2cff9568bdbcd66334d11909
Lego Panda <legopanda2026@gmail.com>
2026-02-23 17:26:03 +0200

    Update runs.py

--- runs.py
+++ runs.py
@@ -21,11 +21,24 @@ def run1():
 
     rob.pid(65, -525)
     wait(300)
-    rob.turn(-30, 125)
+    rob.turn(-25, 125)
     wait(300)
-    arm.run_time(-660, 900)
+    arm.run(-700)
+    wait(400)
+    arm.stop()
     wait(300)
-    rob.pid(3, -200)
+    rob.pid(3, -100)
+    wait(300)
+    arm.run_time(660, 3000)
+    wait(300)
+    rob.pid(6, 100)
+    wait(300)
+    rob.turn(20, 125)
+    wait(300)
+    rob.pid(6, -100)
+    wait(300)
+    rob.shellTurn(70, 400)
+    rob.turn(70, 125)
 
     # rob.pid(10000000, 1000)
 
@@ -50,17 +63,69 @@ def run2():
     wait(200)
     rob.pid(50, 525)
 
-run2()
+# run2()
 
-def run5():
+def run3():
     rob = Robot(kp=0.08, ki=0, kd=0.1, shellKp=2, shellKi=0, shellKd=10, shellTol=0, tol=10, wait_time=1)
 
+    hub.speaker.volume(20)
+    hub.speaker.beep(600, 80)  # Low C
+    wait(100)
+    hub.speaker.beep(800, 80)  # Mid E
+    wait(10)
+    hub.speaker.beep(1100, 120) # High G
+    wait(400)
 
-    rob.pid(70, -525)
-    # rob.AccelDecel(70, -525)
-    wait(300)
+    rob.pid(40, -525)
+    wait(100)
+    arm.run(-1000)
+    wait(1000)
+    arm.stop()
+    wait(100)
+    rob.pid(13, 525)
+    wait(100)
+    arm.run(1000)
+    wait(1300)
+    arm.stop()
+    wait(100)
+    rob.pid(11, -525)
+    wait(100)
+    rob.shellTurn(150, 800)
+    wait(100)
+    rob.pid(30, 525)
+    wait(100)
+    arm.run(700)
+    wait(1300)
+    arm.stop()
+    wait(100)
+    rob.stopColor("stopYellow")
 
-run5()
+# run3()
+
+def run7():
+    rob = Robot(kp=0.08, ki=0, kd=0.1, shellKp=2, shellKi=0, shellKd=10, shellTol=0, tol=10, wait_time=1)
+
+    hub.speaker.volume(20)
+    hub.speaker.beep(600, 80)  # Low C
+    wait(100)
+    hub.speaker.beep(800, 80)  # Mid E
+    wait(10)
+    hub.speaker.beep(1100, 120) # High G
+    wait(400)
+
+    rob.pid(50,-500)
+    wait(400)
+    rob.pid(3,500)
+    wait(400)
+    arm.run_time(660, 1000)
+    wait(400)
+    rob.pid(4,650)
+    wait(200)
+    rob.pid(5, -650)
+    wait(200)
+    # rob.pid(15,525)
+    # wait(200)
+    rob.pid(45, 525)
 
 def battery():
     rob = Robot()
