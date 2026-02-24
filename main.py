39ae67decfe8097ac8fdbaa26596f236a1333d04
Lego Panda <legopanda2026@gmail.com>
2026-02-24 16:57:40 +0200

    Update main.py
    Changed Main

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ hub = PrimeHub(Axis.Y, Axis.Z)
 
 rob = Robot(kp=0.08, ki=0, kd=0.1, shellKp=2, shellKi=0, shellKd=10, shellTol=0, tol=10, wait_time=1)
 
+hub.display.orientation(Side.BOTTOM)
 selected = hub_menu("1", "2", "3", "4", "5", "6", "7", "8", "9")
 
 if selected == "1":
-    run1()
+    test()
 if selected == "2":
     rob.shellButton(degrees=-365)
