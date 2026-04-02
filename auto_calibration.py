162879eb7da1792e71006cf67ce559674b32d46f
Lego Panda <legopanda2026@gmail.com>
2026-04-02 23:34:15 +0300

    Update auto_calibration.py

--- auto_calibration.py
+++ auto_calibration.py
@@ -1,10 +1,4 @@
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
-
-from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop, Axis
 from pybricks.robotics import DriveBase
